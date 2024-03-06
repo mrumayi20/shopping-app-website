4519ae8d4251e612425bbfe2a316afdba88a67ba
Mrunmayi Raju Patil <53402543+mrumayi20@users.noreply.github.com>
2024-03-06 19:22:32 +0000

    Added database connection

--- app.py
+++ app.py
@@ -1,52 +1,23 @@
 from flask import Flask, render_template, jsonify
+from database import load_clothing_from_db
 
 app = Flask(__name__)
 
-ITEMS =[
-  {
-    'id': 1,
-    'title': 'Shirt',
-    'price': '10',
-    'rating': 4.5
-  },
-  {
-    'id': 2,
-    'title': 'Watch',
-    'price': '50',
-    'rating': 4
-  },
-  {
-    'id': 3,
-    'title': 'Shoe',
-    'price': '35',
-    'rating': 5
-  },
-  {
-    'id': 4,
-    'title': 'Shoe',
-    'price': '35',
-    'rating': 5
-  },
-  {
-    'id': 5,
-    'title': 'Shoe',
-    'price': '35',
-    'rating': 5
-  }
-]
 
 @app.route("/")
 def hello_world():
   return render_template('home.html')
 
-# @app.route("/clothing")
-# def clothing():
-#   return render_template('clothing.html', items=ITEMS)
 
 @app.route("/clothing")
 def clothing():
-  return jsonify(ITEMS)#jsonift converts your data into JSON format
-  
+  clothings = load_clothing_from_db()
+  return render_template('clothing.html', clothings=clothings)
+
+
+# @app.route("/clothing")
+# def clothing():
+#   return jsonify(ITEMS)  #jsonift converts your data into JSON format
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
---
+++ database.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine, text
+
+db_connection_string = "mysql+pymysql://rsttjff06sv4ekkvxcnd:pscale_pw_f2y0FhVIpUljYki0fdUhXbqIzCgW5Azt5hg7GcdW801@aws.connect.psdb.cloud/duse?charset=utf8mb4"
+
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                           "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
+
+def load_clothing_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from duse.clothing"))
+    clothings = []
+    for row in result.all():
+      clothings.append(row._mapping)
+  return clothings
+
+
+# print("type(result): ", type(result))
+# result_all = result.all()
+# print("type(result_all): ", type(result_all))
+# print("result_all: ", result_all)
+# print("type(result_all[0]): ", type(result_all[0]))
+# first_result_dict = result_all[0]._mapping
+# print('first_result_dict: ', type(first_result_dict))
+# print(first_result_dict)
+
+# If in section 2.4 of this video is not working for you and you are getting errors like 'Type Error: cannot convert dictionary update sequence element #0 to a sequence,', that just means that you are using SQL Alchemy 1.4 and greater, and dict(result_all[0]) doesn't work in that case, you have to use 'first_result_dict = result_all[0]._mapping'. Hope it will save you sometime. I wasted 2.5 hours on this :))
