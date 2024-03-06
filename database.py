3035f947b16424dabfabe9ffcdf9d9716582cad2
Mrunmayi Raju Patil <53402543+mrumayi20@users.noreply.github.com>
2024-03-06 19:50:20 +0000

    Database connected securely

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ import os
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 
+
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
                            "ssl_ca": "/etc/ssl/cert.pem"
